e2c99f5493817f7f261e4b5659452e6c3abc5887
Sharadinduprakash <Sharadyadav2786@gmail.com>
2022-06-06 17:35:36 +0530

    merge

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,21 +1,22 @@
-import React, { createContext} from "react";
+import React, { createContext, useState } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
 
-  // const [authorized, setAuthorized] = useState(false);
+  const [authorized, setAuthorized] = useState(false);
 
-  // const Login = (username, password) => {
-  //   if(username && password)
-  //   {
-  //     setAuthorized(true)
-  //   }
-  // }
+  const Login = (username, password) => {
+    if(username && password)
+    {
+      setAuthorized(true)
+    }
+  }
 
-  // const Logout = () => {
-  //   setAuthorized(false)
-  // }
+  const Logout = () => {
+    setAuthorized(false)
 
-  return <AuthContext.Provider >{children}</AuthContext.Provider>;
+  }
+
+  return <AuthContext.Provider value={{authorized,Login, Logout}}>{children}</AuthContext.Provider>;
 };
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,10 +1,10 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
 
-  // const [item, setItem] = useState(100);
+  const [item, setItem] = useState(100);
 
-  return <CartContext.Provider>{children}</CartContext.Provider>;
+  return <CartContext.Provider value={{item}}>{children}</CartContext.Provider>;
 };
